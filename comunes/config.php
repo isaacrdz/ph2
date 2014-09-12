80d6d75acd0d398ae094aae2d235c009f926d3e4
isaacrdz <zack_cpp@hotmail.com>
2014-09-12 11:38:31 -0500

    logo

--- comunes/config.php
+++ comunes/config.php
@@ -1,4 +1,4 @@
 <?
-mysql_connect("localhost","zack","123456") or die ("Sin Conexion");
+mysql_connect("localhost","zackpanda","123456") or die ("Sin Conexion");
 mysql_select_db("pandablog");
 ?>
--- comunes/header.php
+++ comunes/header.php
@@ -14,10 +14,7 @@
                             <ul class="social-share">
                                 <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                                 <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li> 
-                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                                <li><a href="#"><i class="fa fa-skype"></i></a></li>
-                            </ul>
+                                                            </ul>
                             <div class="search">
                                 <form role="form">
                                     <input type="text" class="search-form" autocomplete="off" placeholder="Search">
@@ -39,7 +36,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.php">Panda Hominid</a>
+                    <a class="navbar-brand" href="index.php"><img class="logo" src="images/logo.png"  width="50px" alt=""></a>
                 </div>
 				
                 <div class="collapse navbar-collapse navbar-right">
@@ -48,6 +45,7 @@
                         <li><a href="nosotros.php">Nosotros</a></li>
                         <li><a href="servicios.php">Servicios</a></li>
                         <li><a href="portafolio.php">Portafolio</a></li>
+                        <!-- 
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="fa fa-angle-down"></i></a>
                             <ul class="dropdown-menu">
@@ -57,6 +55,7 @@
                                 <li><a href="shortcode.php">Shortcodes</a></li>
                             </ul>
                         </li>
+                         -->
                         <li><a href="blog.php">Blog</a></li> 
                         <li><a href="contacto.php">Contact</a></li>                        
                     </ul>
